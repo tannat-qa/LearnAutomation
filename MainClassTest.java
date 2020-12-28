6501ff160997b08327304dc52d15e603b9cbac86
Tatyana Tsareva <tatyanac@inbox.ru>
2020-12-28 23:54:22 +0300

    Added result Test 2 (Lesson 1)

--- MainClassTest.java
+++ MainClassTest.java
@@ -7,4 +7,9 @@ public class MainClassTest extends MainClass {
         int a = getLocalNumber();
         Assert.assertTrue("The result of getLocalNumber() is not equal 14!",a == 14);
     }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("Private field 'class_number' of MainClass is not great than 45!", getClassNumber() > 45);
+    }
 }
