c5b875f7b626ecafc2d7594bfa05fda09edb8742
Tatyana Tsareva <tatyanac@inbox.ru>
2021-01-14 10:50:07 +0300

    change test code

--- JavaAppiumAutomation/src/FirstTest.java
+++ JavaAppiumAutomation/src/FirstTest.java
@@ -3,7 +3,12 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.net.URL;
 
 public class FirstTest {
@@ -20,8 +25,8 @@ public class FirstTest {
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:/Users/tanya/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
+        //capabilities.setCapability("app", "C:/Users/tanya/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("app", "/Users/tannat/Documents/GitHub/LearnAutomation/JavaAppiumAutomation/apks/org.wikipedia.apk");
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
@@ -33,5 +38,32 @@ public class FirstTest {
     @Test
     public void firstTest() {
         System.out.println("First test run");
+
+        WebElement element_skip = driver.findElementById("org.wikipedia:id/fragment_onboarding_skip_button");
+        element_skip.click();
+        element_skip = driver.findElementById("org.wikipedia:id/view_announcement_action_negative");
+        element_skip.click();
+
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+        element_to_init_search.click();
+        
+        WebElement element_to_enter_search_line = waitForElementPresentByXpath(
+                "//*[contains(@text,'Search Wikipedia')]",
+                "Cannot find search input",
+                5
+        );
+
+        //WebElement element_to_enter_search_line = driver.findElementById("org.wikipedia:id/search_src_text");
+        //element_to_enter_search_line.sendKeys();
+        element_to_enter_search_line.sendKeys("Appium");
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
     }
 }
